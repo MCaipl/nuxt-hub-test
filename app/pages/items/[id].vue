607e012b0c7b3e503e677a5cfb24355c7f10849c
Mathias Caipl <mathias.caipl@gmail.com>
2025-11-01 10:37:34 +0100

    Refactor code structure for improved readability and maintainability

--- app/pages/items/[id].vue
+++ app/pages/items/[id].vue
@@ -22,29 +22,74 @@ const drop = [
   { field: 'Profit Factor', value: '27' },
 ];
 
+const builds = ref([
+  {
+    name: 'Abelard',
+    description: 'benjamincanac',
+    avatar: {
+      src: '/img/abelard.jpg',
+      alt: 'Abelard'
+    }
+  },
+  {
+    name: 'Argenta',
+    description: 'benjamincanac',
+    avatar: {
+      src: '/img/argenta.jpg',
+      alt: 'Argenta'
+    }
+  },
+  {
+    name: 'Pasqal',
+    description: 'benjamincanac',
+    avatar: {
+      src: '/img/pasqal.jpg',
+      alt: 'Pasqal'
+    }
+  },
+  {
+    name: 'Yrliet',
+    description: 'benjamincanac',
+    avatar: {
+      src: '/img/yrliet.jpg',
+      alt: 'benjamincanac'
+    }
+  }
+]);
+
 </script>
 <template>
   <UPage>
     <UPageHeader :title="data?.name" :headline="data?.itemSlotName"/>
     <UPageBody>
       <div class="grid grid-cols-2 gap-6">
-        <!-- <WeaponStats v-if="data" :data="stats"/> -->
+        <WeaponStats v-if="data" :data="stats"/>
         <DropLocation v-if="data" :data="drop"/>
       </div>
 
       <h2 class="relative text-2xl text-highlighted font-bold mt-12 mb-6">Description</h2>
       <p class="leading-7 text-pretty">{{ data?.description }}</p>
 
-     <h2 class="relative text-2xl text-highlighted font-bold mt-12 mb-6">Builds</h2>
-      <!-- <UPageColumns>
-        <UPageCard v-for="i in 7" :key="i"
-            variant="outline"
-            title="Cloudflare's Workers LaunchPad"
-            description="NuxtHub is part of the Cloudflare's Workers Launchpad Cohort to make sure you get a first-class experience on top of Cloudflare's network."
-        />
-      </UPageColumns> -->
+
     </UPageBody>
 
+
+    <template #right>
+      <UPageList divided >
+        <UPageCard
+          v-for="(build, index) in builds"
+          :key="index"
+          variant="soft"
+          spotlight
+          spotlight-color="primary"
+          class="mb-4"
+        >
+          <template #body>
+            <UUser :name="build.name" :description="build.description" :avatar="build.avatar" size="xl" class="relative" />
+          </template>
+        </UPageCard>
+      </UPageList>
+    </template>
   </UPage>
 </template>
 
